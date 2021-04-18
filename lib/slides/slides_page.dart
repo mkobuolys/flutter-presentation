528ba153eecac38881f614dc200dba69e1bd6cf3
Mangirdas Kazlauskas <mangirdas@kazlauskas.dev>
2021-04-18 14:09:54 +0300

    Add keyboard shortcuts to change slides

--- lib/slides/slides_page.dart
+++ lib/slides/slides_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import '1_title/title_slide.dart';
 import '2_agenda/agenda_slide.dart';
@@ -8,35 +9,125 @@ import 'drawbacks_of_flutter/drawbacks_of_flutter_slide.dart';
 import 'rive/rive_slide.dart';
 import 'thank_you/thank_you_slide.dart';
 
-class SlidesPage extends StatelessWidget {
+class SlidesPage extends StatefulWidget {
   const SlidesPage();
+
+  @override
+  _SlidesPageState createState() => _SlidesPageState();
+}
+
+class _SlidesPageState extends State<SlidesPage> {
+  static const _slideSwitchDuration = Duration(milliseconds: 500);
+  static const _slideSwitchCurve = Curves.easeInOut;
+  static const _slidesCount = 7;
+
+  final _pageController = PageController();
+
+  late final Map<LogicalKeySet, Intent> _shortcuts;
+  late final Map<Type, Action<Intent>> _actions;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _shortcuts = <LogicalKeySet, Intent>{
+      LogicalKeySet(LogicalKeyboardKey.alt, LogicalKeyboardKey.arrowLeft):
+          const _HandleSlideIntent.arrowLeft(),
+      LogicalKeySet(LogicalKeyboardKey.alt, LogicalKeyboardKey.arrowRight):
+          const _HandleSlideIntent.arrowRight(),
+    };
+    _actions = <Type, Action<Intent>>{
+      _HandleSlideIntent: CallbackAction<_HandleSlideIntent>(
+        onInvoke: _handleShortcut,
+      ),
+    };
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  void _handleShortcut(_HandleSlideIntent intent) {
+    switch (intent.button) {
+      case _KeyboardButton.arrowLeft:
+        setState(() {
+          if (_pageController.page != 0) {
+            _pageController.previousPage(
+              duration: _slideSwitchDuration,
+              curve: _slideSwitchCurve,
+            );
+          }
+        });
+
+        break;
+      case _KeyboardButton.arrowRight:
+        setState(() {
+          if (_pageController.page! < _slidesCount - 1) {
+            _pageController.nextPage(
+              duration: _slideSwitchDuration,
+              curve: _slideSwitchCurve,
+            );
+          }
+        });
+
+        break;
+      default:
+        break;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PageView.builder(
-        itemBuilder: (context, index) {
-          switch (index) {
-            case 0:
-              return const TitleSlide();
-            case 1:
-              return const AgendaSlide();
-            case 2:
-              return const AboutMeSlide();
-            case 3:
-              return const CustomUiSlide();
-            case 4:
-              return const RiveSlide();
-            case 5:
-              return const DrawbacksOfFlutterSlide();
-            case 6:
-              return const ThankYouSlide();
-          }
+      body: FocusableActionDetector(
+        shortcuts: _shortcuts,
+        actions: _actions,
+        child: Focus(
+          autofocus: true,
+          child: PageView.builder(
+            controller: _pageController,
+            itemBuilder: (context, index) {
+              switch (index) {
+                case 0:
+                  return const TitleSlide();
+                case 1:
+                  return const AgendaSlide();
+                case 2:
+                  return const AboutMeSlide();
+                case 3:
+                  return const CustomUiSlide();
+                case 4:
+                  return const RiveSlide();
+                case 5:
+                  return const DrawbacksOfFlutterSlide();
+                case 6:
+                  return const ThankYouSlide();
+              }
 
-          return const SizedBox();
-        },
-        itemCount: 7,
+              return const SizedBox();
+            },
+            itemCount: _slidesCount,
+          ),
+        ),
       ),
     );
   }
+}
+
+class _HandleSlideIntent extends Intent {
+  final _KeyboardButton button;
+
+  const _HandleSlideIntent({
+    required this.button,
+  });
+
+  const _HandleSlideIntent.arrowLeft() : button = _KeyboardButton.arrowLeft;
+  const _HandleSlideIntent.arrowRight() : button = _KeyboardButton.arrowRight;
+}
+
+enum _KeyboardButton {
+  arrowLeft,
+  arrowRight,
 }
