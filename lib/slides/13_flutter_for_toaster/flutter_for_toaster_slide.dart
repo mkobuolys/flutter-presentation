1a2f4fe54248133296a5e3ec7ffb47874e52a40d
Mangirdas Kazlauskas <mangirdas@kazlauskas.dev>
2021-04-26 21:56:02 +0300

    Add Flutter for Toaster, comparison slides

--- lib/slides/13_flutter_for_toaster/flutter_for_toaster_slide.dart
+++ lib/slides/13_flutter_for_toaster/flutter_for_toaster_slide.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/widgets.dart';
 
+import '../../common/bullet_list.dart';
 import '../../templates/templates.dart';
 
 class FlutterForToasterSlide extends StatelessWidget {
@@ -7,12 +8,21 @@ class FlutterForToasterSlide extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const SplitScreenTemplate(
-      title: 'Flutter for toaster',
-      leftChild: SizedBox(),
-      rightChild: SizedBox(),
-      leftFlex: 2,
-      rightFlex: 3,
+    return CarouselTemplate(
+      title: 'Flutter for Toaster',
+      leftChild: const BulletList(
+        items: [
+          'Cars',
+          'TVs',
+          'Microcomputers',
+          '(You name it) - probably coming soon',
+        ],
+      ),
+      carouselChildren: [
+        Image.asset('images/flutter_for_toaster/toyota.png'),
+        Image.asset('images/flutter_for_toaster/apple_tv.gif'),
+        Image.asset('images/flutter_for_toaster/raspberry_pi.jpeg'),
+      ],
     );
   }
 }
--- lib/slides/14_flutter_comparison/flutter_comparison_slide.dart
+++ lib/slides/14_flutter_comparison/flutter_comparison_slide.dart
@@ -10,8 +10,14 @@ class FlutterComparisonSlide extends StatelessWidget {
   Widget build(BuildContext context) {
     return CarouselTemplate(
       title: 'Flutter vs React Native',
-      leftChild: const BulletList(items: []),
+      leftChild: const BulletList(
+        items: [
+          'Skia vs JavaScript Bridge',
+          'React Native - larger community, but for how long?',
+        ],
+      ),
       carouselChildren: [
+        Image.asset('images/flutter_vs_rn/rn_architecture.png'),
         Image.asset('images/flutter_vs_rn/google_trends.png'),
         Image.asset('images/flutter_vs_rn/has_flutter_passed_rn.png'),
       ],
