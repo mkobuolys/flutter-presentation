4ef1522c442d2970347dddbf45c1760b2523a257
Mangirdas Kazlauskas <mangirdas@kazlauskas.dev>
2021-04-28 01:24:51 +0300

    Add Dart and Tooling slides

--- lib/slides/14_dart/dart_slide.dart
+++ lib/slides/14_dart/dart_slide.dart
@@ -1,16 +1,49 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:youtube_player_iframe/youtube_player_iframe.dart';
 
+import '../../common/bullet_list.dart';
 import '../../templates/templates.dart';
 
-class DartSlide extends StatelessWidget {
+class DartSlide extends StatefulWidget {
   const DartSlide();
 
   @override
+  _DartSlideState createState() => _DartSlideState();
+}
+
+class _DartSlideState extends State<DartSlide> {
+  late final YoutubePlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = YoutubePlayerController(initialVideoId: '5F-6n_2XWR8');
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const SplitScreenTemplate(
+    return SplitScreenTemplate(
       title: 'Dart',
-      leftChild: SizedBox(),
-      rightChild: SizedBox(),
+      leftChild: const BulletList(
+        items: [
+          'JIT and AOT compilation',
+          'Optimized for UI',
+          'Fast on all platforms',
+          'Sound null safety',
+        ],
+      ),
+      rightChild: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          YoutubePlayerIFrame(controller: _controller),
+          const SizedBox(height: 16.0),
+          Text(
+            'Why Flutter uses Dart',
+            style: Theme.of(context).textTheme.headline4,
+          ),
+        ],
+      ),
       leftFlex: 2,
       rightFlex: 3,
     );
--- lib/slides/15_tooling/tooling_slide.dart
+++ lib/slides/15_tooling/tooling_slide.dart
@@ -1,5 +1,6 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 
+import '../../common/bullet_list.dart';
 import '../../templates/templates.dart';
 
 class ToolingSlide extends StatelessWidget {
@@ -7,10 +8,27 @@ class ToolingSlide extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const SplitScreenTemplate(
+    return SplitScreenTemplate(
       title: 'Tooling',
-      leftChild: SizedBox(),
-      rightChild: SizedBox(),
+      leftChild: const BulletList(
+        items: [
+          'Flutter SDK',
+          'Dart DevTools',
+          'Multiple IDE support',
+          'Xcode for Apple platform-specific tasks',
+        ],
+      ),
+      rightChild: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset('images/dart_dev_tools.gif'),
+          const SizedBox(height: 16.0),
+          Text(
+            'Dart DevTools',
+            style: Theme.of(context).textTheme.headline4,
+          ),
+        ],
+      ),
       leftFlex: 2,
       rightFlex: 3,
     );
